d088241f77a351e6af63ed09be85118bb5e22226
Cynigu <isavoxina@mail.ru>
2021-06-13 03:24:13 +0300

    Добавлено окно ПЗ

---
+++ kurs/view/WindowPZwithEditAndUO.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kurs.view
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowPZwithEditAndUO.xaml
+    /// </summary>
+    public partial class WindowPZwithEditAndUO : Window
+    {
+
+        public WindowPZwithEditAndUO()
+        {
+            InitializeComponent();
+        }
+
+        private void Tdokaz_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(Char.IsDigit(e.Text, 0)))
+            {
+                e.Handled = true;
+            }
+        }
+        private void Kdokaz_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(Char.IsDigit(e.Text, 0)))
+            {
+                e.Handled = true;
+            }
+        }
+        private void Period_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(Char.IsDigit(e.Text, 0)))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
